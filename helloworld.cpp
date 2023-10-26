d3ab5ef158c2964c135a74bc029e55a62ed6006a
roberts0001 <147741370+roberts0001@users.noreply.github.com>
2023-10-26 17:57:51 +0200

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+   /* vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+
+    int skaitlis = 3;
+    cout << "ludzu ievadiet skaitli";
+    cin >> skaitlis;
+    cout << skaitlis;
+    if (1 == 1) {
+        cout << "you did it!!!!!!!";
+    }
+
+string virkne;
+cin >> virkne;
+cout << endl << virkne;
+*/
+
+cout << strlen("bfvbgwbgbow")%2;
+}
+
+
